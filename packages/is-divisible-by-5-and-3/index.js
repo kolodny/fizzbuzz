312a252c70d793030dd98d0a4d4950519a4f5378
Moshe Kolodny <kolodny.github@gmail.com>
2016-03-27 19:05:59 -0400

    reuse code

--- packages/is-divisible-by-5-and-3/index.js
+++ packages/is-divisible-by-5-and-3/index.js
@@ -1,5 +1,8 @@
+var isDivisibleBy5 = require("is-divisible-by-5");
+var isDivisibleBy3 = require("is-divisible-by-3");
+
 module.exports = isDivisibleBy5and3;
 
 function isDivisibleBy5and3(number) {
-  return number % 5 === 0 && number % 3 === 0;
+  return isDivisibleBy5(number) && isDivisibleBy3(number);
 }
